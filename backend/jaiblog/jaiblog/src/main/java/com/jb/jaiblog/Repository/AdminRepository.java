ef83a5787d5d6dd88390f6ce703a996f80ecd9a3
jayanthan5 <jayanthan512.j@gmail.com>
2026-03-31 12:08:33 +0530

    updata create post function

--- backend/jaiblog/jaiblog/src/main/java/com/jb/jaiblog/Repository/AdminRepository.java
+++
@@ -1,7 +0,0 @@
-package com.jb.jaiblog.Repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.jb.jaiblog.Entity.Admin;
-
-public interface AdminRepository extends JpaRepository<Admin, Integer> {
-    Admin findByEmailAndPassword(String email,String password);
-}
